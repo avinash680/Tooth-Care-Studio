f475e394edb1a1cb36ef8b5e3fa432af62504b0e
Avinash <avinash13233@gmail.com>
2026-06-28 18:36:38 +0530

    i change some file

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://tooth-care-studio-l1mi.vercel.app/";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://tooth-care-studio-l1mi.vercel.app";
 
 
